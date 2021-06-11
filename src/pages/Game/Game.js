f75a937f645dd22a816346a9f8486272fc7cd11b
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-11 09:59:23 -0500

    Updated my handleClick method by moving it to Game container to update that state

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -2,10 +2,28 @@ import React from 'react';
 import Fingering from '../../components/Fingering/Fingering';
 
 class Game extends React.Component {
+    state = {
+        guess: []
+    }
+    handleClick = (event) => {
+        let newGuess;
+        let { guess } = this.state;
+        let key = event.target;
+        if (!guess.includes(key.id)) {
+            key.style.fill = 'purple';
+            newGuess = guess.concat(key.id);
+            newGuess.sort(); 
+        } else {
+            key.style.fill = 'white';
+            newGuess = guess.filter(guessKey => guessKey !== key);
+        }
+        this.setState({guess: newGuess});
+    }
+    
     render() {
         return (
             <div>
-                <Fingering active={true}/>
+                <Fingering active={true} handleClick={this.handleClick} />
             </div>
         )
     }
