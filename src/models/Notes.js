aa67be33dce5b052774db987efdc2cb798a33a45
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-22 15:38:43 -0500

    Updated urls

--- src/models/Notes.js
+++ src/models/Notes.js
@@ -1,4 +1,4 @@
-const url = 'https://bassoon-finger-trainer-api.herokuapp.com/notes';
+const url = 'https://bassoon-finger-trainer-api.herokuapp.com/api/v1/notes';
 
 class NoteModel {
     static getAllNotes = () => {
