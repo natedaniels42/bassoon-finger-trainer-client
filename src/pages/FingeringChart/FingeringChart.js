8729250f4c0950fdf07b2f95f56b20ed123638d8
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-22 16:45:16 -0500

    CSS tweaks

--- src/pages/FingeringChart/FingeringChart.js
+++ src/pages/FingeringChart/FingeringChart.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Notes from '../../components/Notes/Notes';
-
+import '../../App.css';
 class FingeringChart extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="fingering-chart">
                 <h1>This is the fingering chart</h1>
+                <p>Click a note to see the fingering. If there are multiple fingerings click the buttons below to look at the different options</p>
                 <Notes />
             </div>
         )
