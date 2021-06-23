50c68ded19acce37b815a015e8a81f1a848368d7
Nathan Daniels <nathandanielsbassoon@gmail.com>
2021-06-23 17:45:51 -0500

    Updated text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to Bassoon Finger Trainer</h1>
-            <p>This is a game to train you on your fingerings for the bassoon.  You can choose your level or a secific range you would like to practice. Click on the correct keys to add it to your guess and once you have chosen all the keys click the submit button.  You have 10 fingerings to guess.  Good luck!!!</p>
+            <p>This is a game to train you on your fingerings for the bassoon.  You can choose your level or a specific range you would like to practice. Click on the correct keys to add it to your guess and once you have chosen all the keys click the submit button.  You have 10 fingerings to guess.  Good luck!!!</p>
             <Link to="/levels">
                 <button>Play Game</button>
             </Link>
